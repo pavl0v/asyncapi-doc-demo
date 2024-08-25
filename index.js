e5570a425e07cd4d5d66193ddcf17f5efa8aa258
pavl0v <pavlov.alexei78@gmail.com>
2024-08-25 17:25:20 +0300

    Add function to generate api doc to public/doc folder

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const generateAsyncApiDoc = async function() {
+    const Generator = require('@asyncapi/generator');
+    const path = require('path');
+    const generator = new Generator('@asyncapi/html-template', path.resolve(__dirname, 'public/doc'));
+    try {
+        await generator.generateFromFile('schema.yml');
+        console.log('Documentation generated');
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+generateAsyncApiDoc();
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
